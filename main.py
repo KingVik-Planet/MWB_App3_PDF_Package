667cf3c997e0710d8837d736c692f05e8cacc77b
KingVik-Planet <chukwukingsley56@gmail.com>
2023-11-25 21:48:13 +0100

    update

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ pdf.cell(w=0, h= 12, txt = "How are you", align="C", ln =1, border=1)
 pdf.set_font(family="Helvetica",style= "B", size = 12)
 pdf.cell(w=0, h= 12, txt = "Kingsley Chika CHUKWU", align="R", ln =1, border=1)
 
+
 pdf.add_page()
 pdf.set_font(family="Arial",style= "B", size = 12)
 pdf.cell(w=0, h= 12, txt = "Hi Kingsley", align="L", ln =1, border=1)
