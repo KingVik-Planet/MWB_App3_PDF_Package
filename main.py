6988f53916ae29dd9942ecc051be8cc58f2bcc7c
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-03 13:05:08 +0100

    update

--- main.py
+++ main.py
@@ -15,12 +15,12 @@ pdf.cell(w=0, h= 12, txt = "Kingsley Chika CHUKWU", align="R", ln =1, border=1)
 
 pdf.add_page()
 pdf.set_font(family="Arial",style= "B", size = 12)
-pdf.cell(w=0, h= 12, txt = "Hi Kingsley", align="L", ln =1, border=1)
+pdf.cell(w=0, h= 12, txt = "Hi Kingsley, this is me to you", align="L", ln =1, border=1)
 
 pdf.set_font(family="Times",style= "B", size = 12)
-pdf.cell(w=0, h= 12, txt = "How are you", align="C", ln =1, border=1)
+pdf.cell(w=0, h= 12, txt = "How are you, Hopefully I know you are doing great", align="C", ln =1, border=1)
 
 pdf.set_font(family="Helvetica",style= "B", size = 12)
-pdf.cell(w=0, h= 12, txt = "Kingsley Chika CHUKWU", align="R", ln =1, border=1)
+pdf.cell(w=0, h= 12, txt = "Kingsley Chika CHUKWU, This is the last text in the second page", align="R", ln =1, border=1)
 
 pdf.output("Kingsley.pdf")
