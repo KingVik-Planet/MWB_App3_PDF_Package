8057bc2f43d7425c27be057d1461d1a59159abf3
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-03 13:18:22 +0100

    update

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ pdf = FPDF(orientation= "P", unit="mm", format= "A4")
 
 pdf.add_page()
 pdf.set_font(family="Arial",style= "B", size = 12)
-pdf.cell(w=0, h= 12, txt = "Hi Kingsley", align="L", ln =1, border=1)
+text = """ Streamlit makes it easy for you to visualize, mutate, and share data\n. 
+        The API reference is organized by activity type, like displaying data \n
+        or optimizing performance. Each section includes methods associated with \n
+        the activity type, including examples.)"""
+
+pdf.cell(w=0, h= 12, txt = text, align="L", ln =1, border=1)
 
 pdf.set_font(family="Times",style= "BI", size = 12)
 pdf.cell(w=0, h= 12, txt = "How are you", align="C", ln =1, border=1)
