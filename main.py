abac93d52c6a4ffb226b2039ff88c24e7484cbb9
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-04 14:11:40 +0100

    Update

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 from fpdf import FPDF
 import pandas as pd
 
-
 pdf = FPDF(orientation= "P", unit="mm", format= "A4")
 
-df = pd.read_csv("topic.csv")
+df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
         pdf.add_page()
 
-        pdf.set_font(family="Helvetica",style= "B", size = 12)
-        pdf.set_text_color(r=100, g=100, b=100)
-        pdf.cell(w=0, h= 12, txt = "Kingsley Chika CHUKWU", align="R", ln =1, border=1)
+        pdf.set_font(family="Helvetica",style= "B", size = 25)
+        pdf.set_text_color(r=100, g=10, b=100)
+        pdf.cell(w=0, h= 12, txt = row["Topic"], align="L", ln =1, border=0)
         pdf.line(x1=10, y1=20, x2=200, y2=20)
-
-
 
 pdf.output("Kingsley.pdf")
