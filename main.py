3ff3438dc6b088234749aaa3bc5d953231446c00
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-13 15:05:05 +0100

    Update

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 from fpdf import FPDF
 import pandas as pd
 
-pdf = FPDF(orientation= "P", unit="mm", format= "A4")
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=Fagit lse, margin=0)
 
 df = pd.read_csv("topics.csv")
 
-for index, row in df.iterrows():
+for index,row in df.iterrows():
+    pdf.add_page()
+
+    #Header guidelines
+    pdf.set_font(family="Times", style="B", size=24)
+    pdf.set_text_color(100, 10, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
+    pdf.line(10, 20, 200, 20)
+
+    #Footer guidelines
+    pdf.ln(260)
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(20, 20, 200)
+    pdf.cell(w=0, h=10, txt=row['Topic'], align= "R")
+
+    for i in range(row["Pages"] - 1):
         pdf.add_page()
 
-        pdf.set_font(family="Helvetica",style= "B", size = 25)
-        pdf.set_text_color(r=100, g=10, b=100)
-        pdf.cell(w=0, h= 12, txt = row["Topic"], align="L", ln =1, border=0)
-        pdf.line(x1=10, y1=20, x2=200, y2=20)
+        #Footer Guidelines
+        pdf.ln(272)
 
-for i in range(row["Pages"] -1):
-    pdf.add_page()
+        pdf.set_font(family="Times", style="B", size=12)
+        pdf.set_text_color(20, 20, 200)
+        pdf.cell(w=0, h=10, txt=row['Topic'], align="R")
 
 pdf.output("Kingsley.pdf")
