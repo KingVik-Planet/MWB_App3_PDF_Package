c208e42f992d7030079cb76c82fa0d7ecf8a5b6f
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-13 15:07:23 +0100

    Changes on the page

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fpdf import FPDF
 import pandas as pd
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
-pdf.set_auto_page_break(auto=Fagit lse, margin=0)
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
